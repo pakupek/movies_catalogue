9c337588e3094c6cc58a6da0cd6ba78d9ae593db
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-06-09 12:59:40 +0200

    update1

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import json
 import requests
 import tmdb_client
 
+
 app = Flask(__name__)
+'''
+def movie_details(movie_id):
+'''
 
 
 
@@ -19,7 +23,10 @@ def homepage():
     movies = tmdb_client.get_movies(how_many=4)
     return render_template("homepage.html", movies=movies)
 
-
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    movie = movie.get(movie_id)
+    return render_template("movie_details.html", movies_id=movie_id)
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,20 +1,21 @@
 import requests
 
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular/"
-    apitoken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzEwMzY4MmY0ZWQ0NTI3Y2QzYjYzZDM1NTcyZmU3MiIsInN1YiI6IjYyOWY4ODY3ZDIxNDdjMTE3ZTYzZTNlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rmqYp6McgetjqfoH6uXJzJqlyBwVTZVyG8qTlY0glw8"
-    headers = {
-        "Authorization": f"Bearer {apitoken}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json
 
+def get_popular_movies():
+    end_point = "https://api.themoviedb.org/3/movie/popular/"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzEwMzY4MmY0ZWQ0NTI3Y2QzYjYzZDM1NTcyZmU3MiIsInN1YiI6IjYyOWY4ODY3ZDIxNDdjMTE3ZTYzZTNlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rmqYp6McgetjqfoH6uXJzJqlyBwVTZVyG8qTlY0glw8"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(end_point, headers=headers)
+    return response.json()
+
+get_popular_movies()
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
 
-
 def get_movies(how_many):
     data = get_popular_movies()
-    return data["results"][:how_many]
+    return data["results"][:how_many] 
