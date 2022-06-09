a1d05f98eaeb2cf6d1cf99c0842a5bfcb840d4e8
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-06-09 21:15:48 +0200

    add diff type list

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
-
+import random
 
 app = Flask(__name__)
-
-
 
 
 @app.context_processor
@@ -16,14 +14,18 @@ def utility_processor():
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=4)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=4, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
+
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
     details = tmdb_client.get_single_movie(movie_id)
     cast = tmdb_client.get_single_movie_cast(movie_id)
-    return render_template("movie_details.html", movie=details, cast=cast)
+    movie_image = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_image['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -3,12 +3,13 @@ import requests
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzEwMzY4MmY0ZWQ0NTI3Y2QzYjYzZDM1NTcyZmU3MiIsInN1YiI6IjYyOWY4ODY3ZDIxNDdjMTE3ZTYzZTNlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rmqYp6McgetjqfoH6uXJzJqlyBwVTZVyG8qTlY0glw8"
 API_TOKEN2 = "87103682f4ed4527cd3b63d35572fe72"
 
-def get_popular_movies():
-    end_point = "https://api.themoviedb.org/3/movie/popular/"
+def get_movies_list(list_type='popular'):
+    end_point = f"https://api.themoviedb.org/3/movie/{list_type}/"
     headers = {
         "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(end_point, headers=headers)
+    response.raise_for_status()
     return response.json()
 
 
@@ -17,8 +18,8 @@ def get_poster_url(poster_api_path, size="w342"):
     return f"{base_url}{size}/{poster_api_path}"
 
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
     return data["results"][:how_many] 
 
 
@@ -39,4 +40,11 @@ def get_single_movie_cast(movie_id):
     response = requests.get(endpoint, headers=headers)
     return response.json()["cast"]
 
-    
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images" 
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json() 
